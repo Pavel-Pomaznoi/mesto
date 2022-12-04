54513b8fd276c3978fb4c70d019fd358023ad9ba
Pavel Pomaznoi <gondorf1@yandex.ru>
2022-12-04 20:34:08 +0300

    Закончена работа для отправки на ревью

---
+++ pages/script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+let openModal = document.querySelector('.profile__button-edit'),
+    modal = document.querySelector('.popup'),
+    modalContainer = document.querySelector('.popup__container'),
+    closeModal = modal.querySelector('.popup__close-button'),
+    formEditProfile = document.querySelector('.popup__form'),
+    inputName = formEditProfile.querySelector('.popup__item_type_name'),
+    inputSignature = formEditProfile.querySelector('.popup__item_type_about'),
+    saveButton = formEditProfile.querySelector('.popup__botton'),
+    profileTitle = document.querySelector('.profile__title'),
+    profileSubtitle = document.querySelector('.profile__subtitle');
+
+function formSubmitHandler(e) {
+  e.preventDefault();
+  profileTitle.textContent = inputName.value;
+  profileSubtitle.textContent = inputSignature.value;
+  hideModal();
+
+}
+
+function showModal() {
+  inputName.value = profileTitle.textContent;
+  inputSignature.value = profileSubtitle.textContent;
+  modal.classList.add('popup_opened');
+
+}
+
+function hideModal() {
+  modal.classList.remove('popup_opened');
+
+}
+
+openModal.addEventListener('click', showModal);
+closeModal.addEventListener('click', hideModal);
+formEditProfile.addEventListener('submit', formSubmitHandler);
+modal.addEventListener('click', (e) => {
+  if(e.target === e.currentTarget) {
+    hideModal();
+
+  }
+
+})
